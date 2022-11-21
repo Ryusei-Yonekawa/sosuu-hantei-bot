b49c0f3af51465406cb47142d9968446aea53ed1
米川竜世 <yonekawaryusei@yonekawaryuuseinoMacBook-Air.local>
2022-11-21 14:36:23 +0900

    make it better

--- app.py
+++ app.py
@@ -1,47 +1,41 @@
-# ライブラリをインポート
 from flask import Flask, request, abort
 from linebot import (
-    LineBotApi, WebhookHandler
+ LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-    InvalidSignatureError
+ InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage
+ MessageEvent, TextMessage, TextSendMessage
 )
-import os
-#Flaskを準備
+
 app = Flask(__name__)
-#環境変数からLINE Access Tokenを設定
-LINE_CHANNEL_ACCESS_TOKEN = os.environ["1657669164"]
-#環境変数からLINE Channel Secretを設定
-LINE_CHANNEL_SECRET = os.environ["6373d78a3d704292f81c3feb2a805b20"]
-#LineBotApiのインスタンスを生成
-line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
-#WebhookHandlerのインスタンスを生成
-webhook_handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+ACCESS_TOKEN = "1657669164"
+SECRET = "6373d78a3d704292f81c3feb2a805b20"
+
+line_bot_api = LineBotApi(ACCESS_TOKEN)
+handler = WebhookHandler(SECRET)
+
 @app.route("https://git.heroku.com/sosuu-hantei-bot.git/callback", methods=['POST'])
 def callback():
-    # HTTPリクエストヘッダからX-Line-Signatureを取り出す
-    signature = request.headers['X-Line-Signature']
-    #テキストでpostされたデータを取得
+    signature = request.headers['X-Line-Signature']  
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    # webhookのbodyを解析する
- #この結果はadd関数で受け取る
- #なお、Signatureが一致していない時はInvalidSignatureError例外が発生する
+
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
+
     return 'OK'
-# MessageEvent
-@handler.add(MessageEvent, message=TextMessage) #イベントに対する返信をここに書く
+
+@handler.add(MessageEvent, message=TextMessage)#ここでイベントに対する応答をする
 def handle_message(event):
-    #テキストでの返信を行う
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text ) #ユーザーが送信したメッセージを取得
-    )
+ line_bot_api.reply_message(
+  event.reply_token,
+  TextSendMessage(text=event.message.text))
+
 if __name__ == "__main__":
-    app.run()
+ app.run()
+
